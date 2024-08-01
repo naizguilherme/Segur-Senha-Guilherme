2469d9b56cb23bab13aa35b8d2316ccd02b9d3ed
Guilherme Naiz <108024770+naizguilherme@users.noreply.github.com>
2024-08-01 17:38:22 -0300

    01/08

--- main.js
+++ main.js
@@ -5,8 +5,39 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho
 
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha - 1;
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1;
+        numeroSenha.textContent = tamanhoSenha;
+    }
     numeroSenha.textContent = tamanhoSenha;
 }
+
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+    tamanhoSenha = tamanhoSenha + 1;}
+    numeroSenha.textContent = tamanhoSenha;
+
+}
+
+const campoSenha = document.querySelector('#campo=senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ;
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz' ;
+const numeros = '123456789' ;
+const simbolos = '!@%*?' ;
+
+
+geraSenha();
+
+function geraSenha() {
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) 
+       let numeroAleatorio = math.random() * letrasMaiusculas.length
+       nu b  
+}
+
+
+
